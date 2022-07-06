f0084092af9b10639656b532c47c07675a711bc9
braincee <kwesi938@gmail.com>
2022-07-06 21:21:09 +0100

    Add verification before rollback and commits

--- queries.sql
+++ queries.sql
@@ -21,12 +21,14 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 ROLLBACK;
+SELECT * FROM animals;
 
 -- another animal transaction
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
 COMMIT;
 
 
