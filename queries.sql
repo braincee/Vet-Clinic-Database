a4c3d74fe4764b1527dd8ce6748a8b223291b06a
braincee <kwesi938@gmail.com>
2022-07-06 18:56:08 +0100

    Perform delete savepoint & update transactions

--- queries.sql
+++ queries.sql
@@ -36,3 +36,13 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 
+-- another delete transaction
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SAVEPOINT_1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT_1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
