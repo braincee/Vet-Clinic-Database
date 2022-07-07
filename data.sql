2ec8eb02172c7f2c08380520671f3c5fdcd8d362
braincee <kwesi938@gmail.com>
2022-07-07 11:35:11 +0100

    Modify inserted animals to include owner information

--- data.sql
+++ data.sql
@@ -89,3 +89,29 @@ VALUES ('Pokemon'),
 -- Modifying inserted animals so it includes the species_id:
 -- if the name ends in "mon" it will be Digimon
 UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+
+-- all other animals are Pokemon
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
+
+-- Modify your inserted animals to include owner information (owner_id):
+
+-- Sam Smith owns Agumon
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+
+-- Jennifer Orwell owns Gabumon and Pikachu
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+-- Bob owns Devimon and Plantmon
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name = 'Devimon' OR name = 'Plantmon';
+
+-- Melody Pond owns Charmander, Squirtle, Blossom
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+-- Dean Winchester owns Angemon and Boarmon
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name = 'Angemon' OR name = 'Boarmon';
