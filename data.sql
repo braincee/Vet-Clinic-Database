d7b8cedb217a83d8cad493540c23f015242eae2f
braincee <kwesi938@gmail.com>
2022-07-07 10:28:37 +0100

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -68,3 +68,14 @@ VALUES ('Charmander', '2020-02-08', 0, False, -11),
         ('Boarmon', '2005-06-07', 7, True, 20.4),
         ('Blossom', '1998-10-13', 3, True, 17),
         ('Ditto', '2022-05-14', 4, True, 22);
+
+
+-- inserting data into owners table
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
