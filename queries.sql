845b141e67276803eab15dd6ba0930161ba6acd6
braincee <kwesi938@gmail.com>
2022-12-05 00:26:59 +0000

    add optional chnages

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-SELECT name FROM animals WHERE neutered = True AND escape_attempts < 3;
+SELECT name FROM animals WHERE neutered = 'true' AND escape_attempts < '3';
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
@@ -12,7 +12,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 
 SELECT * FROM animals WHERE neutered = True;
 
-SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
